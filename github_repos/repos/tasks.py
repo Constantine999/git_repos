21dcb72719caba8a7d338b1973a028e69dfdc411
Constantine999 <desperado904@gmail.com>
2024-09-27 15:21:00 +0300

    debugging the code

--- github_repos/repos/tasks.py
+++ github_repos/repos/tasks.py
@@ -1,14 +1,13 @@
 # celery tasks module
-import asyncio
+
 import importlib
 import time
 from typing import NewType, Optional
-import json
+
 import aiohttp
 import celery
-import requests
+from asgiref.sync import async_to_sync
 from celery.result import AsyncResult
-from django.utils.asyncio import async_unsafe
 
 CELERY_RESULT_BACKEND = importlib.import_module("github_repos.settings").CELERY_RESULT_BACKEND
 
@@ -22,49 +21,35 @@ app = celery.Celery(
 )
 
 
-# @app.task(expires=5)
-# async def get_repositories_by_username(username: str) -> GithubData | ErrorMessage:
-#     url = f"https://api.github.com/users/{username}/repos"
-#     response = requests.get(url)
-#     if data := response.json():
-#         repos = [
-#             {"name": repo["name"], "stars": repo["stargazers_count"]}
-#             for repo in data
-#         ]
-#
-#         return repos
-#     return {"Error": f"Пользователь с именем {username} - не зарегистрирован на github"}
+async def async_get_data_repositories_by_username(username: str) -> GithubData:
+    url = f"https://api.github.com/users/{username}/repos"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url) as response:
+            return await response.json()
+
 
 @app.task(expires=5)
-async def get_repositories_by_username(username: str) -> GithubData | ErrorMessage:
-    pass
-    # url = f"https://api.github.com/users/{username}/repos"
-    # async with aiohttp.ClientSession() as session:
-    #     async with session.get(url=url) as response:
-    #         # data = await response.json()
-    #         data = {"name": 1, "stargazers_count": 2}
-    #         if data:
-    #             return [
-    #                 {"name": repo["name"], "stars": repo["stargazers_count"]}
-    #                 for repo in data
-    #             ]
-    #
-    # return {"Error": f"Пользователь с именем {username} - не зарегистрирован на github"}
+def get_data_repositories_by_username(username: str) -> GithubData | ErrorMessage:
+    data = async_to_sync(async_get_data_repositories_by_username)(username)
+    if data:
+        return [
+            {"name": repo["name"], "stars": repo["stargazers_count"]} for repo in data
+        ]
+
+    return {"Error": f"Пользователь с именем {username} - не зарегистрирован на github"}
 
 
-@async_unsafe
-def get_celery_result_by_task_id(task_id: str) -> Optional[GithubData]:
-    pass
-#     if task_id:
-#         timeout = 10
-#         task: AsyncResult = celery.result.AsyncResult(id=task_id)
-#         try:
-#             result = task.get(timeout=timeout)
-#             while result.status != "SUCCESS":
-#                 with open("status.txt", "a", encoding="utf-8") as file:
-#                     print(result.status, file=file)
-#             return result
-#         except celery.exceptions.TimeoutError:
-#             return {"Error": f"Превышен лимит запроса в размере {timeout} секунд"}
-#
-#     return None
+def get_celery_result_by_task_id(task_id: str, timeout: int = 6) -> Optional[GithubData]:
+    if task_id:
+        task: AsyncResult = AsyncResult(id=task_id)
+        start = time.perf_counter()
+        status = ""
+
+        try:
+            while status != "SUCCESS" and time.perf_counter() - start < timeout:
+                status = task.status
+            return task.result
+        except celery.exceptions.TimeoutError:
+            return {"Error": f"Превышен лимит запроса в размере {timeout} секунд"}
+
+    return None
--- github_repos/repos/views.py
+++ github_repos/repos/views.py
@@ -1,44 +1,37 @@
-import asyncio
 from typing import Optional
 
-from asgiref.timeout import timeout
-from celery.result import AsyncResult
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect
 from django.utils.asyncio import async_unsafe
 
-from .tasks import get_repositories_by_username, get_celery_result_by_task_id, GithubData
+from .tasks import get_data_repositories_by_username, get_celery_result_by_task_id, GithubData
 
 
-async def index(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
+@async_unsafe
+def index(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST" and request.POST["username"]:
         github_user = request.POST["username"]
-        task_id: Optional[str] = get_repositories_by_username.apply_async(args=[github_user]).id
-        # request.session["task_id"] = task_id
-        await asyncio.to_thread(lambda: setattr(request.session, "task_id", task_id))
+        task_id: Optional[str] = get_data_repositories_by_username.apply_async(args=[github_user]).id
+        request.session["task_id"] = task_id
+        return redirect("result")
 
-        return await asyncio.to_thread(redirect, "result")
-    return await asyncio.to_thread(
-        render,
+    return render(
         request=request,
         template_name="repos/index.html",
     )
 
 
-async def result(request: HttpRequest) -> HttpResponse:
-    # task_id: str = request.session.get("task_id")
-    task_id: str = await asyncio.to_thread(request.session.get, "task_id")
+@async_unsafe
+def result(request: HttpRequest) -> HttpResponse:
+    task_id: str = request.session.get("task_id")
     result: Optional[GithubData] = get_celery_result_by_task_id(task_id=task_id)
-
-
     if "Error" not in result:
-        return await asyncio.to_thread(
-            render,
+        return render(
             request=request,
             template_name="repos/result.html",
             context={"result": result},
         )
-    return await asyncio.to_thread(
-        HttpResponse,
+
+    return HttpResponse(
         content=result["Error"],
     )
