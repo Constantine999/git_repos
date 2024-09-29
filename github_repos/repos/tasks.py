1d3a1528cace5ca7eb40178089bef5ae65fb78bf
Constantine999 <desperado904@gmail.com>
2024-09-29 12:03:59 +0300

    refactor code

--- github_repos/repos/tasks.py
+++ github_repos/repos/tasks.py
@@ -1,43 +1,17 @@
-import time
-
-import aiohttp
 import celery
-from asgiref.sync import async_to_sync, sync_to_async
-from celery.result import AsyncResult
+import requests
 
 from .core import GithubData, ErrorMessage
 
 
-async def async_get_data_repositories_by_username(username: str) -> GithubData:
-    url = f"https://api.github.com/users/{username}/repos"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url=url) as response:
-            return await response.json()
-
-
 @celery.shared_task(time_limit=3)
 def get_data_repositories_by_username(username: str) -> GithubData | ErrorMessage:
-    data = async_to_sync(async_get_data_repositories_by_username)(username)
-    if data:
+    url = f"https://api.github.com/users/{username}/repos"
+    response = requests.get(url=url)
+
+    if data := response.json():
         return [
             {"name": repo["name"], "stars": repo["stargazers_count"]} for repo in data
         ]
 
     return {"Error": f"Пользователь с именем {username} - не зарегистрирован на github"}
-
-
-@sync_to_async
-def get_celery_result_by_task_id(task_id: str, timeout: int) -> GithubData | ErrorMessage | None:
-    if task_id:
-        task: AsyncResult = AsyncResult(id=task_id)
-        start = time.perf_counter()
-        status = ""
-
-        try:
-            while status != "SUCCESS" and time.perf_counter() - start < timeout:
-                status = task.status
-            return task.result
-        except celery.exceptions.TimeoutError:
-            return {"Error": f"Превышен лимит запроса в размере {timeout} секунд"}
-
-    return None
--- github_repos/repos/views.py
+++ github_repos/repos/views.py
@@ -1,10 +1,13 @@
-from typing import Optional
+import time
 
-from asgiref.sync import sync_to_async, async_to_sync
+import celery
+from asgiref.sync import sync_to_async
+from celery.result import AsyncResult
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect
 
-from .tasks import get_data_repositories_by_username, get_celery_result_by_task_id, GithubData
+from .core import TIMEOUT
+from .tasks import get_data_repositories_by_username
 
 
 @sync_to_async
@@ -24,14 +27,23 @@ def index(request: HttpRequest) -> HttpResponse:
 @sync_to_async
 def result(request: HttpRequest) -> HttpResponse:
     task_id: str = request.session.get("task_id")
-    result: Optional[GithubData] = async_to_sync(get_celery_result_by_task_id)(task_id, 4)
-    if "Error" not in result:
-        return render(
-            request=request,
-            template_name="repos/result.html",
-            context={"result": result},
-        )
 
-    return HttpResponse(
-        content=result["Error"],
-    )
+    if task_id:
+        task: AsyncResult = AsyncResult(id=task_id)
+
+        try:
+            start = time.perf_counter()
+            while task.status != "SUCCESS" and time.perf_counter() - start < TIMEOUT:
+                continue
+
+            return render(
+                request=request,
+                template_name="repos/result.html",
+                context={"result": task.result},
+            )
+
+        except celery.exceptions.TimeoutError:
+            return HttpResponse(
+                content=f"Превышен лимит запроса в размере {TIMEOUT} секунд")
+
+    return None
